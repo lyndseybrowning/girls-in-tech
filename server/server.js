6c9c28fcccc89b3ef4ff59c4b5d9ab37adb460c6
Lyndsey Browning <lbrowning86@gmail.com>
2020-02-16 18:04:55 +0000

    Add port to env file

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ const connect = require("./connection");
 const initialiseRoutes = require("./initialiseRoutes");
 
 const ROOT_DIR = "src";
-const PORT = 3000;
 const app = express();
 
 if (process.env.NODE_ENV !== "production") {
@@ -13,6 +12,8 @@ if (process.env.NODE_ENV !== "production") {
 connect(collection => {
     initialiseRoutes(app, collection);
 
-    app.listen(PORT, () => console.log(`listening on port ${PORT}`));
+    app.listen(process.env.PORT, () =>
+        console.log(`listening on port ${process.env.PORT}`),
+    );
     app.use(express.static(ROOT_DIR));
 }).catch(console.error);
